4d8d34f65d2a0337ad43dd2e950f9e8a15dbc466
John <John@vpn149a.rzuser.uni-heidelberg.de>
2017-03-10 10:30:57 +0100

    osm building samples

--- statistics/osm_missing.py
+++ statistics/osm_missing.py
@@ -1,8 +1,7 @@
 #! /usr/bin/python
 
-#test
-
 import sys
+import random
 
 sys.path.append("../lib")
 import MapSwipe
@@ -14,21 +13,46 @@ matched = []
 missed = []
 
 osm_lines = FileIO.read_lines("../data/buildings.csv", 1)
-osm_tasks = []
+osm_imgs = []
 for line in osm_lines:
     tmp = line.strip().split(',')
-    osm_tasks.append(tmp[5] + '_' + tmp[6])
-osm_tasks = sorted(osm_tasks)
+    osm_imgs.append(tmp[5] + ',' + tmp[6])
+osm_imgs = sorted(osm_imgs)
 
 p_imgs = client.read_p_images()
 for i in p_imgs:
-    p_img = str(i[0]) + '_' + str(i[1])
-    if p_img in osm_tasks:
+    p_img = str(i[0]) + ',' + str(i[1])
+    if p_img in osm_imgs:
         print '%s matched by OSM' % p_img
         matched.append(p_img)
     else:
         print '%s missed by OSM' % p_img
         missed.append(p_img)
 
+print '-----------------------random images start----------------------------'
+n = 200
+samples = []
+L = 50
+
+matched_r = random.sample(matched, n)
+for img in matched_r:
+    for line in osm_lines:
+        if img in line:
+            tmp = line.strip().split(',')
+            x, y = int(tmp[-2]), int(tmp[-1])
+            if x - L / 2 <= 0 or y - L / 2 <= 0 or x + L / 2 > 256 or y + L / 2 > 256:
+                continue
+            else:
+                s = 'yes,%s,%d,%d\n' % (img, x, y)
+                samples.append(s)
+
+missed_r = random.sample(missed, n)
+for img in missed_r:
+    s = 'no,%s\n' % img
+    samples.append(s)
+
+print '%d random samples' % FileIO.save_lines("../data/building_samples.csv", samples)
+print '-----------------------random images end----------------------------'
+
 print '%d out of %d images are missed by OSM' % (len(missed), len(p_imgs))
-print 'missing rate: %f' % float(len(missed)/float(len(p_imgs)))
+print 'missing rate: %f' % float(len(missed) / float(len(p_imgs)))
