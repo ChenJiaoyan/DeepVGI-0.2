0c455fbbf5847460768748a8a952070e5c76ad9c
yan <zhouyan090@gmail.com>
2017-03-14 11:11:22 +0100

    zy

--- bin/classify_urban_shp.py
+++ bin/classify_urban_shp.py
@@ -2,46 +2,54 @@
 
 import sys
 import csv
+import numpy as np
 from osgeo import ogr
 
-in_file = "../data/shp/project_922.shp"
-out_file = "../data/malawi_urban.csv"
-shp_file = "../data/shp/malawi_urban.shp"
+in_file = "shp/project_922.shp"
+out_file = "malawi_urban.csv"
+shp_file = "malawi_urban.shp"
 
-
-def get_Layer(shp_name):
-    driver = ogr.GetDriverByName("ESRI Shapefile")
-    source = driver.Open(shp_name, 0)
-    layer = source.GetLayer()
-    #layer_defn = layer.GetLayerDefn()
-    return layer
+driver = ogr.GetDriverByName("ESRI Shapefile")
+source1 = driver.Open(in_file, 0)
+layer1 = source1.GetLayer()
+source2 = driver.Open(shp_file, 0)
+layer2 = source2.GetLayer()
 
 all_922 = []
 all_urban = []
+item = []
+for feature1 in layer1:
+    geometry = feature1.GetGeometryRef()
+    all_922.append(geometry.Clone())
 
-for feature1 in get_Layer(in_file):
-    geometry1 = feature1.GetGeometryRef()
-    all_922.append(geometry1.Clone())
+    task_x = float(feature1.GetField("task_x"))
+    task_y = float(feature1.GetField("task_y"))
+    xy = str(task_x) + ',' + str(task_y)
+    item.append(xy)
+matrix = np.array((all_922, item))
 
-for feature2 in get_Layer(shp_file):
-    geometry2 = feature2.GetGeometryRef()
-    all_urban.append(geometry2.Clone())
+for feature2 in layer2:
+    geometry = feature2.GetGeometryRef()
+    all_urban.append(geometry.Clone())
 
+# layer_defn = layer.GetLayerDefn()
 f = open(out_file, 'w')
 f.write('task_x' + ',' + 'task_y' + ',' + 'classification' + '\n')
-
+m = 0;
+n = 0
 for urban in all_urban:
-    for tile in all_922:
-        for feature in get_Layer(in_file):
-            if urban.Intersect(tile) == True :
-                task_x = feature.GetField("task_x")
-                task_y = feature.GetField("task_y")
-                item = task_x + ',' + task_y + ',' + 'urban' + '\n'
-                f.write(item)
-            else:
-                task_x = feature.GetField("task_x")
-                task_y = feature.GetField("task_y")
-                item = task_x + ',' + task_y + ',' + 'rural' + '\n'
-                f.write(item)
+    for i in range(len(all_922)):
+        if urban.Intersect(matrix[0][i]):
+            line = matrix[1][i] + ',' + 'urban' + '\n'
+            m += 1
+            f.write(line)
+        else:
+            line = matrix[1][i] + ',' + 'rural' + '\n'
+            n += 1
+            f.write(line)
 
 f.close()
+source1.Destroy()
+source2.Destroy()
+
+print ("Total urban tile: %s \nTotal rural tiles: %s" % (m, n))
