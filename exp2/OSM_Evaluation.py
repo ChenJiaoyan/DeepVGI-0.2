fd617c48af214c416a71a0461a9a68a56e0c0216
John <John@vpn066a.rzuser.uni-heidelberg.de>
2017-03-22 12:45:49 +0100

    add args processing

--- exp2/OSM_Evaluation.py
+++ exp2/OSM_Evaluation.py
@@ -69,11 +69,11 @@ def deal_args(my_argv):
             print 'OSM_Evaluation.py -n1 <p_sample_size> -n0 <n_sample_size> -b <batch_size>'
             sys.exit()
         elif opt in ("-n1", "--p_sample_size"):
-            n1 = arg
+            n1 = int(arg)
         elif opt in ("-n0", "--n_sample_size"):
-            n0 = arg
+            n0 = int(arg)
         elif opt in ("-b", "--batch_size"):
-            b = arg
+            b = int(arg)
     print 'settings: n1=%d, n0=%d, b=%d' % (n1, n0, b)
     return n1, n0, b
 
