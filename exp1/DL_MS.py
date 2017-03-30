31126b4c78a9fd011f1c9d81d2aafb3341930d16
yan <zhouyan090@gmail.com>
2017-03-30 16:16:17 +0200

    zy

--- exp1/DL_MS.py
+++ exp1/DL_MS.py
@@ -69,7 +69,7 @@ def read_train_sample(n1, n0, train_imgs, ms_p_imgs, ms_n_imgs):
 
 
 def deal_args(my_argv):
-    v, n1, n0, b, e, t, c, z = False, 100, 100, 30, 1000, 8, 0, 1000
+    v, n1, n0, b, e, t, c, z = False, 100, 100, 10, 100, 3, 0, 50
     m = 'lenet'
     try:
         opts, args = getopt.getopt(my_argv, "vhy:n:b:e:t:c:z:m:",
