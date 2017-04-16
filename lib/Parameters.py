6052094af7780256432213e32c7ef952ec258364
John <John@vpn088a.rzuser.uni-heidelberg.de>
2017-04-16 09:41:58 +0200

    add another parameter -d (use external test images)

--- lib/Parameters.py
+++ lib/Parameters.py
@@ -5,21 +5,23 @@ import sys
 
 
 def deal_args(my_argv):
-    v, n1, n0, b, e, t, c, z = False, 200, 200, 30, 1000, 8, 0, 1000
+    v, d, n1, n0, b, e, t, c, z = False, False, 200, 200, 30, 1000, 8, 0, 1000
     m = 'lenet'
     try:
-        opts, args = getopt.getopt(my_argv, "vhy:n:b:e:t:c:z:m:",
+        opts, args = getopt.getopt(my_argv, "vdhy:n:b:e:t:c:z:m:",
                                    ["p_sample_size=", "n_sample_size=", "batch_size=", "epoch_num=", "thread_num=",
                                     "cv_round=", 'test_size=', 'network_model='])
     except getopt.GetoptError:
-        print 'DL_MS.py -v -y <p_sample_size> -n <n_sample_size> -b <batch_size> -e <epoch_num> -t <thread_num>, ' \
+        print 'DL_MS.py -v -d -y <p_sample_size> -n <n_sample_size> -b <batch_size> -e <epoch_num> -t <thread_num>, ' \
               '-c <cv_round>, -z <test_size>, -m <network_model>'
-        print 'default settings: v=%s, n1=%d, n0=%d, b=%d, e=%d, t=%d, c=%d, z=%d, m=%s' % (v, n1, n0, b, e, t, c, z, m)
+        print 'default settings: v=%s, d=%s, n1=%d, n0=%d, b=%d, e=%d, t=%d, c=%d, z=%d, m=%s' % (v, d, n1, n0, b, e, t, c, z, m)
     for opt, arg in opts:
         if opt == '-h':
-            print 'DL_MS.py -v -y <p_sample_size> -n <n_sample_size> -b <batch_size> -e <epoch_num> -t <thread_num>, ' \
+            print 'DL_MS.py -v -d -y <p_sample_size> -n <n_sample_size> -b <batch_size> -e <epoch_num> -t <thread_num>, ' \
                   '-c <cv_round>, -z <test_size>, -m <network_model>'
             sys.exit()
+        elif opt == '-d':
+            d = True
         elif opt == '-v':
             v = True
         elif opt in ("-y", "--p_sample_size"):
@@ -38,5 +40,5 @@ def deal_args(my_argv):
             z = int(arg)
         elif opt in ("-m", "--network_model"):
             m = arg
-    print 'settings: v=%s, n1=%d, n0=%d, b=%d, e=%d, t=%d, c=%d, z=%d, m=%s' % (v, n1, n0, b, e, t, c, z, m)
-    return v, n1, n0, b, e, t, c, z, m
+    print 'settings: v=%s, d=%s, n1=%d, n0=%d, b=%d, e=%d, t=%d, c=%d, z=%d, m=%s' % (v, d, n1, n0, b, e, t, c, z, m)
+    return v, d, n1, n0, b, e, t, c, z, m
