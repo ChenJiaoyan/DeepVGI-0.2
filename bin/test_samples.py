97b97e3c185a4f4dadd27f188ea60502cac56400
John <John@John-Pro.local>
2017-06-11 01:24:47 +0200

    extract test image files

--- bin/test_samples.py
+++ bin/test_samples.py
@@ -11,23 +11,26 @@ import os
 if __name__ == '__main__':
     a_imgs = FileIO.read_lines("../data/test_imgs.csv", 0)
     p_imgs = FileIO.read_lines("../data/test_positive_imgs.csv", 0)
+    a_imgs = [img.strip for img in a_imgs]
+    p_imgs = [img.strip for img in p_imgs]
+
     n_imgs = list(set(a_imgs).difference(set(p_imgs)))
 
     imagery = os.listdir('../data/imagery')
 
     print 'moving file for test/Positive'
     for img in p_imgs:
-        if img.strip() in imagery:
+        if img in imagery:
             shutil.copyfile(os.path.join('../data/imagery', img),
                             os.path.join('../samples0/test/Positive/',
-                                         img.strip().replace('_18.', '.').replace('_', '-')))
+                                         img.replace('_18.', '.').replace('_', '-')))
         else:
             print '%s does NOT have image file' % img
 
     for img in n_imgs:
-        if img.strip() in imagery:
+        if img in imagery:
             shutil.copy(os.path.join('../data/imagery', img),
                         os.path.join('../samples0/test/Negative/',
-                                     img.strip().replace('_18.', '.').replace('_', '-')))
+                                     img.replace('_18.', '.').replace('_', '-')))
         else:
             print '%s does NOT have image file' % img
