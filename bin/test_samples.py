a0d6b4f1cc6b4861229b8f41d0370847a6316396
John <John@John-Pro.local>
2017-06-11 01:25:26 +0200

    extract test image files

--- bin/test_samples.py
+++ bin/test_samples.py
@@ -11,8 +11,8 @@ import os
 if __name__ == '__main__':
     a_imgs = FileIO.read_lines("../data/test_imgs.csv", 0)
     p_imgs = FileIO.read_lines("../data/test_positive_imgs.csv", 0)
-    a_imgs = [img.strip for img in a_imgs]
-    p_imgs = [img.strip for img in p_imgs]
+    a_imgs = [img.strip() for img in a_imgs]
+    p_imgs = [img.strip() for img in p_imgs]
 
     n_imgs = list(set(a_imgs).difference(set(p_imgs)))
 
