fc067295eb90fcc30b6ae1be95b0527e2b6c1835
John <John@John-Pro.local>
2017-06-10 23:32:50 +0200

    re-write read train/test sample()

--- exp1/DL_MS.py
+++ exp1/DL_MS.py
@@ -11,71 +11,76 @@ from scipy import misc
 
 sys.path.append("../lib")
 import NN_Model
-import FileIO
 import MapSwipe
 
-def read_test_sample(n, test_imgs, ms_p_imgs, ms_n_imgs):
-    img_X = np.zeros((n, 256, 256, 3))
+sample_dir = '../samples0/'
+
+
+def read_test_sample(n):
+    client = MapSwipe.MSClient()
+    MS_valid_p = client.MS_valid_positive()
+    MS_valid_n = client.MS_valid_negative()
+
+    print 'MS_valid_p: %d \n' % len(MS_valid_p)
+    print 'MS_valid_n: %d \n' % len(MS_valid_n)
+
+    if len(MS_valid_p) <= n/2 or len(MS_valid_p) <= n/2:
+        print 'n is set too large; use all the samples for testing'
+        n = len(MS_valid_p) * 2
+
+    img_X1, img_X0 = np.zeros((n/2, 256, 256, 3)), np.zeros((n/2, 256, 256, 3))
+    MS_valid_p = random.sample(MS_valid_p, n/2)
+    for i, img in enumerate(MS_valid_p):
+        img_X1[i] = misc.imread(os.path.join(sample_dir, 'valid/MS_positive/', img))
+
+    MS_valid_n = random.sample(MS_valid_n, n/2)
+    for i, img in enumerate(MS_valid_n):
+        img_X1[i] = misc.imread(os.path.join(sample_dir, 'valid/MS_negative/', img))
+
+    X = np.concatenate((img_X1[0:n/2], img_X0[0:n/2]))
+
     label = np.zeros((n, 2))
-    img_dir1 = '../data/image_project_922/'
-    img_dir2 = '../data/image_project_922_negative/'
-    random.shuffle(test_imgs)
-    i = 0
-    te_p, te_n = 0, 0
-    for img in test_imgs:
-        if i >= n:
-            break
-        if os.path.exists(os.path.join(img_dir1, img)):
-            img_X[i] = misc.imread(os.path.join(img_dir1, img))
-        else:
-            img_X[i] = misc.imread(os.path.join(img_dir2, img))
-        i1, i2 = img.index('-'), img.index('.')
-        task_x, task_y = img[0:i1], img[(i1 + 1):i2]
-        if [int(task_x), int(task_y)] in ms_p_imgs:
-            label[i, 1] = 1
-            i += 1
-            te_p += 1
-        elif [int(task_x), int(task_y)] in ms_n_imgs:
-            label[i, 0] = 1
-            i += 1
-            te_n += 1
-    print 'positive testing samples: %d \n ' % te_p
-    print 'negative testing samples: %d \n ' % te_n
-    return img_X, label
+    label[0:n/2, 1] = 1
+    label[n/2:n, 0] = 1
 
-## read train samples from MapSwipe (Positive and Negative)
-def read_train_sample(n1, n0, train_imgs, ms_p_imgs, ms_n_imgs):
+    return X, label
+
+
+## Positive: MapSwipe,  Negative: MapSwipe
+def read_train_sample(n1, n0):
+    client = MapSwipe.MSClient()
+    MS_train_p = client.MS_train_positive()
+    MS_train_n = client.MS_train_negative()
+
+    print 'MS_train_p: %d \n' % len(MS_train_p)
+    print 'MS_train_n: %d \n' % len(MS_train_n)
+
+    if len(MS_train_p) <= n1:
+        print 'n1 is set too large'
+        sys.exit()
+
+    if len(MS_train_n) <= n0:
+        print 'n0 is set too large'
+        sys.exit()
+
     img_X1, img_X0 = np.zeros((n1, 256, 256, 3)), np.zeros((n0, 256, 256, 3))
+
+    MS_train_p = random.sample(MS_train_p, n1)
+    for i, img in enumerate(MS_train_p):
+        img_X1[i] = misc.imread(os.path.join(sample_dir, 'train/MS_positive/', img))
+
+    MS_train_n = random.sample(MS_train_n, n0)
+    for i, img in enumerate(MS_train_n):
+        img_X0[i] = misc.imread(os.path.join(sample_dir, 'train/MS_negative/', img))
+
+    X = np.concatenate((img_X1[0:n1], img_X0[0:n0]))
+
     label = np.zeros((n1 + n0, 2))
-    img_dir1 = '../data/image_project_922/'
-    img_dir2 = '../data/image_project_922_negative/'
-
-    ms_po_imgs, ms_ne_imgs = [], []
-    for img in train_imgs:
-        i1, i2 = img.index('-'), img.index('.')
-        task_x, task_y = img[0:i1], img[(i1 + 1):i2]
-        k =  [int(task_x), int(task_y)]
-        if k in ms_p_imgs:
-            ms_po_imgs.append(img)
-        elif k in ms_n_imgs:
-            ms_ne_imgs.append(img)
-
-    print 'ms_po_imgs: %d \n' % len(ms_po_imgs)
-    print 'ms_ne_imgs: %d \n' % len(ms_ne_imgs)
-
-    ms_po_imgs = random.sample(ms_po_imgs, n1)
-    for i, img in enumerate(ms_po_imgs):
-        img_X1[i] = misc.imread(os.path.join(img_dir1, img))
     label[0:n1, 1] = 1
-
-    ms_ne_imgs = random.sample(ms_ne_imgs, n0)
-    for i, img in enumerate(ms_ne_imgs):
-        img_X0[i] = misc.imread(os.path.join(img_dir2, img))
     label[n1:(n1 + n0), 0] = 1
 
     j = range(n1 + n0)
     random.shuffle(j)
-    X = np.concatenate((img_X1, img_X0))
     return X[j], label[j]
 
 
@@ -119,33 +124,23 @@ def deal_args(my_argv):
 
 if __name__ == '__main__':
     evaluate_only, tr_n1, tr_n0, tr_b, tr_e, tr_t, cv_i, te_n, nn = deal_args(sys.argv[1:])
-    cv_n = 4
 
     print '--------------- Read Samples ---------------'
-    client = MapSwipe.MSClient()
-    train_imgs, test_imgs = client.imgs_cross_validation(cv_i, cv_n)
-    ms_p_imgs = client.read_p_images()
-    ms_n_imgs = client.read_n_images()
-    print 'train_imgs: %d \n' % len(train_imgs)
-    print 'ms_p_imgs: %d \n' % len(ms_p_imgs)
-    print 'ms_n_imgs: %d\n' % len(ms_n_imgs)
-    img_X, Y = read_train_sample(tr_n1, tr_n0, train_imgs, ms_p_imgs, ms_n_imgs)
-    m = NN_Model.Model(img_X, Y, nn + '_ZY')
+    img_X, Y = read_train_sample(tr_n1, tr_n0)
 
+    m = NN_Model.Model(img_X, Y, nn + '_ZY')
     if not evaluate_only:
-        print '--------------- Training on MS Labels---------------'
+        print '--------------- Training ---------------'
         m.set_batch_size(tr_b)
         m.set_epoch_num(tr_e)
         m.set_thread_num(tr_t)
         m.train(nn)
         print '--------------- Evaluation on Training Samples ---------------'
         m.evaluate()
-    del img_X, Y, train_imgs
+    del img_X, Y
     gc.collect()
 
-    print '--------------- Evaluation on MapSwipe Samples ---------------'
-    ms_p_imgs = client.read_p_images()
-    ms_n_imgs = client.read_n_images()
-    img_X2, Y2 = read_test_sample(te_n, test_imgs, ms_p_imgs, ms_n_imgs)
+    print '--------------- Evaluation on Validation Samples ---------------'
+    img_X2, Y2 = read_test_sample(te_n)
     m.set_evaluation_input(img_X2, Y2)
     m.evaluate()
