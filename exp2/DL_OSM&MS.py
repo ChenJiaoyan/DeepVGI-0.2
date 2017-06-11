5c5aa0953282ce5b1be469751048235585a5703a
John <John@John-Pro.local>
2017-06-11 20:27:07 +0200

    rewrite sampling in DL_OSM&MS.py

--- exp2/DL_OSM&MS.py
+++ exp2/DL_OSM&MS.py
@@ -91,6 +91,6 @@ if __name__ == '__main__':
         print '--------------- Evaluation on Expert  Labeled Samples ---------------'
         img_X3, Y3 = FileIO.read_external_test_sample()
         m.set_evaluation_input(img_X3, Y3)
-        m.evaluate(True)
+        m.evaluate()
         del img_X3, Y3
         gc.collect()
