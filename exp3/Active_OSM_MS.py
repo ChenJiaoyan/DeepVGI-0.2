420df80a613a29086dc1cdaa29ff557ea5b4d97f
John <John@John-Pro.local>
2017-06-13 22:01:31 +0200

    read train samples

--- exp3/Active_OSM_MS.py
+++ exp3/Active_OSM_MS.py
@@ -16,10 +16,50 @@ import FileIO
 import MapSwipe
 import Parameters
 
-sys.path.append("../exp2")
-import DL_OSM_MS
-
 sample_dir = '../samples0/'
+
+
+def read_train_sample(n1, n0):
+    client = MapSwipe.MSClient()
+    MS_train_n = client.MS_train_negative()
+    MS_train_r = client.MS_train_record()
+    MS_train = MS_train_r + MS_train_n
+
+    task_w = FileIO.osm_building_weight();
+    OSM_train_p = list(set(task_w.keys()).intersection(set(MS_train)))
+    MS_train_n = list(set(MS_train_n).difference(set(OSM_train_p)))
+
+    print 'OSM_train_p: %d \n' % len(OSM_train_p)
+    print 'MS_train_n: %d \n' % len(MS_train_n)
+
+    if len(OSM_train_p) < n1:
+        print 'n1 is set too large'
+        sys.exit()
+
+    if len(MS_train_n) < n0:
+        print 'n0 is set too large'
+        sys.exit()
+
+    img_X1, img_X0 = np.zeros((n1, 256, 256, 3)), np.zeros((n0, 256, 256, 3))
+    OSM_train_p = random.sample(OSM_train_p, n1)
+    for i, img in enumerate(OSM_train_p):
+        if img in MS_train_r:
+            img_X1[i] = misc.imread(os.path.join(sample_dir, 'train/MS_record/', img))
+        else:
+            img_X1[i] = misc.imread(os.path.join(sample_dir, 'train/MS_negative/', img))
+
+    MS_train_n = random.sample(MS_train_n, n0)
+    for i, img in enumerate(MS_train_n):
+        img_X0[i] = misc.imread(os.path.join(sample_dir, 'train/MS_negative/', img))
+
+    label = np.zeros((n1 + n0, 2))
+    label[0:n1, 1] = 1
+    label[n1:(n1 + n0), 0] = 1
+
+    j = range(n1 + n0)
+    random.shuffle(j)
+    X = np.concatenate((img_X1, img_X0))
+    return X[j], label[j]
 
 
 def active_sampling(m0, act_n, t_up, t_low):
@@ -70,7 +110,7 @@ if __name__ == '__main__':
         Parameters.deal_args_active(sys.argv[1:])
 
     print '--------------- Read Samples ---------------'
-    img_X, Y = DL_OSM_MS.read_train_sample(tr_n1, tr_n0)
+    img_X, Y = read_train_sample(tr_n1, tr_n0)
 
     if not evaluate_only:
         print '--------------- M0: Training on OSM Labels---------------'
